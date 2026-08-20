aaf5d0df25d6ebf55d5bc50906c3f10339198236
lalazlm <laura.zelma.alves@escola.pr.gov.br>
2026-08-20 08:24:05 -0300

    salvar

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-principal");
+
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+];
