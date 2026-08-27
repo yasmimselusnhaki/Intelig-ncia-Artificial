88c029f629ab40711d8780113e789b533fb6cd47
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2026-08-27 07:41:17 -0300

    salvar

--- script.js
+++ script.js
@@ -16,18 +16,21 @@ const perguntas = [
     enunciado: "A IA analisa os exames rapidamente e apresenta algumas possibilidades. Porém, Helena percebe que a tecnologia pode cometer erros e que a decisão final ainda depende de um profissional.Como Helena deve utilizar a informação da IA?",
     alternativas: [
       "Comparar a sugestão da IA com sua própria avaliação antes de decidir.", 
-      "Aceitar o resultado da IA sem questioná-lo."],
+      "Aceitar o resultado da IA sem questioná-lo."
+    ],
   },
-   {
+  {
     enunciado: "Depois de analisar as informações, Helena chega a uma possível solução para o caso. Agora, ela precisa conversar com o paciente e explicar como chegou àquela decisão.Qual deve ser a atitude da médica?",
     alternativas: [
       "Explicar o diagnóstico de forma clara e mostrar que a IA foi apenas uma ferramenta de apoio.", 
-      "Dizer apenas que o diagnóstico foi feito pela tecnologia."],
+      "Dizer apenas que o diagnóstico foi feito pela tecnologia."
+    ],
   },
-   {
+  {
     enunciado: "Após o atendimento, Helena percebe que a inteligência artificial pode transformar a medicina. Porém, também entende que tecnologia e conhecimento humano precisam trabalhar juntos.Como Helena deve enxergar a IA na medicina?",
     alternativas: [
       "Como uma ferramenta que auxilia médicos, sem substituir o cuidado humano.", 
-      "Como uma tecnologia capaz de substituir completamente os médicos."],
+      "Como uma tecnologia capaz de substituir completamente os médicos."
+    ],
   },
 ];
