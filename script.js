faa1cefa998f4352baea5a570685ee53b88da80a
lalazlm <laura.zelma.alves@escola.pr.gov.br>
2026-08-27 07:52:07 -0300

    salvar

--- script.js
+++ script.js
@@ -34,3 +34,11 @@ const perguntas = [
     ],
   },
 ];
+
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
