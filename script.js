4559a1830ecdadf968e3f8551e784e36a604a85e
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2026-09-03 08:16:27 -0300

    salvar

--- script.js
+++ script.js
@@ -74,12 +74,12 @@ function mostraAlternativas(){
    for(const alternativa of perguntaAtual.alternativas) {
       const botaoAlternativas = document.createElement("button");
       botaoAlternativas.textContent = alternativa.texto;
-      botaoAlternativas.addEventListener("click", function() => respostaSelecionada(alternativa));
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
       caixaAlternativas.appendChild(botaoAlternativas);
    }
 }
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = opcaoSelecionada.afirmacoes;
     historiaFinal = afirmacoes;
     atual++;
     mostraPergunta();
