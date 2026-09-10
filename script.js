65a5f5572c3c172d3c2d14c4682407653bc89948
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2026-09-10 07:36:44 -0300

    salvar

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function mostraAlternativas(){
    }
 }
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacoes;
+    const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
